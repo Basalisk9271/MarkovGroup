4f9b81c40bca4b60d6f8aee894f265fbd366a478
duke_kb <10996967@live.mercer.edu>
2022-11-29 16:18:06 -0500

    start

---
+++ go/markov.go
@@ -0,0 +1,61 @@
+import ("fmt")
+
+package main
+
+func main() {
+
+}
+
+func markovRange(){
+
+
+}
+
+func gen_markov_sequence(){
+
+}
+
+func collapse(){
+
+}
+
+func toRoman(){
+
+}
+
+/*
+
+//%d for integers
+//%f for floating-point numbers
+
+//User input:
+
+var lower float64
+var upper float64
+
+fmt.Println("Lower: ")
+fmt.Scan(&lower)
+
+fmt.Println("Upper: ")
+fmt.Scan(&upper)
+ 
+Print:
+
+fmt.Println(i, "\n")
+
+
+Main{}
+
+markovRange{}
+
+gen_markov_sequence{}
+
+Collapse{}
+
+toRoman{}
+
+
+ */
+
+
+
