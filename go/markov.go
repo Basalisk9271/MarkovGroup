f5df2fd467fcda1b2739d02db9f53bc96a8ad902
Gabriel A Imlay <gabe.imlay9271@gmail.com>
2022-12-01 20:50:09 -0500

    Fixed the ToRoman Function

--- go/markov.go
+++ go/markov.go
@@ -85,22 +85,7 @@ func toRoman(val int) string {
 		output = "N"	
 	} else {
 		for (val > 0){
-			if(100 <= val){
-				output = output + "L"
-				val -= 100
-			}else if(99 == val){
-				output = output + "IL"
-				val -= 99
-			}else if(50 <= val){
-				output = output + "C"
-				val -= 50
-			}else if(49 == val){
-				output = output + "IC"
-				val -= 49
-			}else if(10 <= val){
-				output = output + "X"
-				val -= 10
-			}else if(9 == val){
+			if(9 == val){
 				output = output + "IX"
 				val -= 9
 			}else if(5 <= val){
