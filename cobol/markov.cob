ae907e4e7d3a41fb61a56c7eb62d8f21c8b17532
tas_ta <tas_ta@cobra.cs.mercer.edu>
2022-12-04 13:09:48 -0500

    cobol with cmdline

--- cobol/markov.cob
+++ cobol/markov.cob
@@ -14,16 +14,37 @@
            01 lower pic 9(2) value 10.
            01 upper pic 9(2) value 50.
            01 cmdline pic x(50).
+           01 arg1 pic x(50).
+           01 arg2 pic x(50).
+
+
+      
+
 
        procedure division.
           
       * This language is actually cringe. * 
-           
+           accept cmdline from command-line.
+           display cmdline.
+           move function trim(cmdline) to cmdline.
+           display cmdline.
+
+           unstring cmdline delimited by space into arg1 arg2.
+           display arg1.
+           display arg2.
+
+
+           compute lower = function numval(arg1).
+           compute upper = function numval(arg2).
+           display "Lower: " lower.
+           display "Upper: " upper.
+
+
            cringe-para.
            display lower. 
            add 1 to lower.
            
-           perform cringe-para until lower>upper.
+           perform cringe-para until lower>upper. 
 
            
 
