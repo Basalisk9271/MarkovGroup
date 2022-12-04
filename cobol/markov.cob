24320aa9edd451a8e159ed1525f190e60b04fa8f
tas_ta <tas_ta@cobra.cs.mercer.edu>
2022-12-04 12:34:57 -0500

    Group started Cobol.

---
+++ cobol/markov.cob
@@ -0,0 +1,33 @@
+       identification division.
+       program-id.
+       markov.
+       author.
+           Caramel Macchiato
+       date-written.
+           December 4, 2022.
+
+       environment division.
+       input-output section.
+
+       data division.
+       working-storage section.
+           01 lower pic 9(2) value 10.
+           01 upper pic 9(2) value 50.
+           01 cmdline pic x(50).
+
+       procedure division.
+          
+      * This language is actually cringe. * 
+           
+           cringe-para.
+           display lower. 
+           add 1 to lower.
+           
+           perform cringe-para until lower>upper.
+
+           
+
+
+
+           stop run.
+
