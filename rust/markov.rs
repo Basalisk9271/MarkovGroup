f682582810d42599e319d6ab3a6be399eab67c47
Ervin E. Pangilinan <Ervin.Escalona.Pangilinan@live.mercer.edu>
2022-12-04 15:07:19 -0500

    Starting Rust

---
+++ rust/markov.rs
@@ -0,0 +1,45 @@
+/*
+CSC 330: Organization of Programming Languages - Fall 2022
+Group Project: Markov Numbers
+Implemented in Rust
+*/
+
+use std::env;
+
+fn markovRange(lower: i64, upper: i64) -> i64 {
+    // PRE: Bounds of the range are passed in. 
+    // POST: Recursively looks for Markov numbers in the range and returns a list of those numbers.
+    
+    
+
+
+}
+
+fn generateMarkov() -> i64 {
+    // PRE:
+    // POST:
+
+
+}
+
+fn collapse() -> i64 {
+    // PRE:
+    // POST:
+
+
+}
+
+fn toRoman() -> String {
+    // PRE:
+    // POST:
+
+
+}
+
+fn main() {
+    // PRE:
+    // POST:
+
+    
+}
+
