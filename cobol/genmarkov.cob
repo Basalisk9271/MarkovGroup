7aef34cad7a7191d8a297485c1f590ac501bff30
Your Name <koenig_mm@cobra.cs.mercer.edu>
2022-12-07 22:50:35 -0500

    Fixed GenMarkov

--- cobol/genmarkov.cob
+++ cobol/genmarkov.cob
@@ -19,46 +19,46 @@
       *    Corresponds to each value of a given Markov triple. *  
            01 a PIC 9(12) COMP-5.
            01 b PIC 9(12) COMP-5.
-           01 c PIC 9(12) COMP-5.
+           01 cval PIC 9(12) COMP-5.
            
-       PROCEDURE DIVISION USING Low, High, M-Table, a, b, c.
+       PROCEDURE DIVISION USING Low, High, M-Table, a, b, cval.
          
       *    Add to table if the c-value is a unique Markov number. *    
-           IF c >= Low AND c <= High THEN  
-               IF 3*a*b*c = a*a + b*b + c*c THEN 
+           IF cval >= Low AND cval <= High THEN  
+               IF 3*a*b*cval = a*a + b*b + cval*cval THEN 
       *    Index variable. *
                    SET I TO 1.
                    SEARCH M-Value 
                        AT END 
-                           MOVE c TO M-Table
-                       WHEN M-Value(I) = c 
+                           MOVE cval TO M-Table
+                       WHEN M-Value(I) = cval 
                            CONTINUE. 
                           
       *    BASE CASE *
-           IF c > High THEN
+           IF cval > High THEN
                EXIT PROGRAM.
        
       *    Tree traversal to jump into the given range. * 
-           IF c < Low AND c <= 2 THEN  
-               COMPUTE Temp = 3*a*c - b.
-               CALL 'genmarkov' USING Low, High, M-Table, a, c, Temp.
+           IF cval < Low AND cval <= 2 THEN  
+               COMPUTE Temp = 3*a*cval - b.
+               CALL 'genmarkov' USING Low, High, M-Table, a, cval, Temp.
 
-           IF c < Low AND c > 2 THEN
-               COMPUTE Temp = 3*a*c - b.
-               CALL 'genmarkov' USING Low, High, M-Table, a, c, Temp.
-               COMPUTE Temp = 3*b*c - a.
-               CALL 'genmarkov' USING Low, High, M-Table, a, c, Temp.
-               COMPUTE Temp = 3*a*b - c.
-               CALL 'genmarkov' USING Low, High, M-Table, b, c, Temp.  
+           IF cval < Low AND cval > 2 THEN
+               COMPUTE Temp = 3*a*cval - b.
+               CALL 'genmarkov' USING Low, High, M-Table, a, cval, Temp.
+               COMPUTE Temp = 3*b*cval - a.
+               CALL 'genmarkov' USING Low, High, M-Table, a, cval, Temp.
+               COMPUTE Temp = 3*a*b - cval.
+               CALL 'genmarkov' USING Low, High, M-Table, b, cval, Temp.
 
-           IF c <= 2 THEN  
+           IF cval <= 2 THEN  
       *    Traverse to the top/next linear node in the tree. *
-               CALL 'genmarkov' USING Low, High, M-Table, a, c, Temp.
+               CALL 'genmarkov' USING Low, High, M-Table, a, cval, Temp.
 
       *    Traverse to the top and bottom subtrees. *
-           CALL 'genmarkov' USING Low, High, M-Table, a, c, Temp.
-           CALL 'genmarkov' USING Low, High, M-Table, a, c, Temp.
-           CALL 'genmarkov' USING Low, High, M-Table, b, c, Temp.
+           CALL 'genmarkov' USING Low, High, M-Table, a, cval, Temp.
+           CALL 'genmarkov' USING Low, High, M-Table, a, cval, Temp.
+           CALL 'genmarkov' USING Low, High, M-Table, b, cval, Temp.
 
        EXIT PROGRAM.
            
