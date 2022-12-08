9eed8330d5b823b4c4d13fb8ab0215b4b5afd4e8
Ervin E. Pangilinan <Ervin.Escalona.Pangilinan@live.mercer.edu>
2022-12-07 22:41:36 -0500

    Fixed some bugs lol

--- cobol/genmarkov.cob
+++ cobol/genmarkov.cob
@@ -8,56 +8,57 @@
 
        DATA DIVISION.
        LOCAL-STORAGE SECTION.
+           01 Temp PIC 9(12) VALUE ZERO.
 
        LINKAGE SECTION.
-           01 Low. PIC 9(12) COMP-5.
-           01 High. PIC 9(12) COMP-5.
+           01 Low PIC 9(12) COMP-5.
+           01 High PIC 9(12) COMP-5.
            01 M-Table.
-               02 M-Value PIC 9(12) OCCURS 150 TIMES.
+               05 M-Value PIC 9(12) OCCURS 150 TIMES INDEXED BY I.
 
       *    Corresponds to each value of a given Markov triple. *  
-           01 a. PIC 9(12) COMP-5.
-           01 b. PIC 9(12) COMP-5.
-           01 c. PIC 9(12) COMP-5.
+           01 a PIC 9(12) COMP-5.
+           01 b PIC 9(12) COMP-5.
+           01 c PIC 9(12) COMP-5.
            
-       PROCEDURE DIVISION USING VALUES Low, High, M-Table, a, b, c.
-
+       PROCEDURE DIVISION USING Low, High, M-Table, a, b, c.
+         
       *    Add to table if the c-value is a unique Markov number. *    
            IF c >= Low AND c <= High THEN  
-               IF 3*a*b*c == a*a + b*b + c*c THEN 
-                   SEARCH M-Table  
-                       WHEN c       
-                           CONTINUE. 
+               IF 3*a*b*c = a*a + b*b + c*c THEN 
+      *    Index variable. *
+                   SET I TO 1.
+                   SEARCH M-Value 
                        AT END 
-                           ADD c TO M-Table.  
-               END-IF      
-           END-IF
-           
+                           MOVE c TO M-Table
+                       WHEN M-Value(I) = c 
+                           CONTINUE. 
+                          
       *    BASE CASE *
            IF c > High THEN
                EXIT PROGRAM.
-           END-IF 
        
       *    Tree traversal to jump into the given range. * 
            IF c < Low AND c <= 2 THEN  
-               CALL 'genmarkov' USING Low, High, M-Table, a, c, 3*a*c-b.
-           END-IF  
+               COMPUTE Temp = 3*a*c - b.
+               CALL 'genmarkov' USING Low, High, M-Table, a, c, Temp.
 
            IF c < Low AND c > 2 THEN
-               CALL 'genmarkov' USING Low, High, M-Table, a, c, 3*a*c-b.
-               CALL 'genmarkov' USING Low, High, M-Table, a, c, 3*b*c-a.
-               CALL 'genmarkov' USING Low, High, M-Table, b, c, 3*a*b-c.
-           END-IF   
+               COMPUTE Temp = 3*a*c - b.
+               CALL 'genmarkov' USING Low, High, M-Table, a, c, Temp.
+               COMPUTE Temp = 3*b*c - a.
+               CALL 'genmarkov' USING Low, High, M-Table, a, c, Temp.
+               COMPUTE Temp = 3*a*b - c.
+               CALL 'genmarkov' USING Low, High, M-Table, b, c, Temp.  
 
            IF c <= 2 THEN  
       *    Traverse to the top/next linear node in the tree. *
-               CALL 'genmarkov' USING Low, High, M-Table, a, c, 3*a*c-b.
-           ELSE 
+               CALL 'genmarkov' USING Low, High, M-Table, a, c, Temp.
+
       *    Traverse to the top and bottom subtrees. *
-               CALL 'genmarkov' USING Low, High, M-Table, a, c, 3*a*c-b.
-               CALL 'genmarkov' USING Low, High, M-Table, a, c, 3*b*c-a.
-               CALL 'genmarkov' USING Low, High, M-Table, b, c, 3*a*b-c.
-           END-IF
+           CALL 'genmarkov' USING Low, High, M-Table, a, c, Temp.
+           CALL 'genmarkov' USING Low, High, M-Table, a, c, Temp.
+           CALL 'genmarkov' USING Low, High, M-Table, b, c, Temp.
 
        EXIT PROGRAM.
            
